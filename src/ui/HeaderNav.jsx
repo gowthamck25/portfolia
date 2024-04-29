00bbcb98dd8ce22941e674c1bbfb1b92caf8cf02
gowthamck25 <gowthamck03@gmail.com>
2024-04-29 22:56:23 +0530

    Resume Update

--- src/ui/HeaderNav.jsx
+++ src/ui/HeaderNav.jsx
@@ -83,7 +83,7 @@ function HeaderNav() {
       <li>
         <HeaderNavStyledNavLink
           target="_blank"
-          to="https://drive.google.com/file/d/1kO26Z5Dfp1ZUltiBBOLBaWqySnok3ez6/view?usp=drive_link"
+          to="https://drive.google.com/file/d/1AHAoyzFIOl0fz5CWugxpJiZzLhN8NakQ/view?usp=drive_link"
         >
           Resume
         </HeaderNavStyledNavLink>
